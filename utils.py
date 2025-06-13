04e17eba0b41877927892e8fc00bf4f56d284ac2
Rihtak007 <emailsforkathir@gmail.com>
2025-06-13 16:07:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import os
 import requests
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
+BOT_TOKEN = os.getenv("7794145234:AAGTiTtmppgfHfMZk4atX6KR9gO7c2Vaoxg")
+TELEGRAM_USER_ID = os.getenv("5410947715")
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     payload = {
-        "chat_id": TELEGRAM_USER_ID,
+        "chat_id": 5410947715,
         "text": message
     }
     requests.post(url, json=payload)
